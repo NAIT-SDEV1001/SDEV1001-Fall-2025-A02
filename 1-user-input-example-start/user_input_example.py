b4242599922771008358fdd53d584ed6d7bd1bdd
Daniel Mouris <dgmouris@gmail.com>
2025-09-08 14:29:12 -0600

    class progress pushed

--- 1-user-input-example-start/user_input_example.py
+++ 1-user-input-example-start/user_input_example.py
@@ -10,7 +10,7 @@ last_name = input("what is your last name? ")
 # print a greeting with all of them together
 # this is with something called concatentation
 # which is a fancy work for adding off these together.
-print("Greeting fine " + first_name + " " + last_name)
+print("Greeting fine " + first_naame + " " + last_name)
 
 # run it in the terminal
 
