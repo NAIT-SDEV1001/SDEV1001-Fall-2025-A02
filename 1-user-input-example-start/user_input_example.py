e5f76006bd8c4b58d17c8c08fb9e67806fd339f5
Daniel Mouris <dgmouris@gmail.com>
2025-09-08 13:46:01 -0600

    class progress pushed

---
+++ 1-user-input-example-start/user_input_example.py
@@ -0,0 +1,16 @@
+# this is big enough
+
+# I want to prompt the user for the last name
+first_name = input("what is your first name? ")
+
+# let's get the last name of the user
+last_name = input("what is your last name? ")
+
+
+# print a greeting with all of them together
+# this is with something called concatentation
+# which is a fancy work for adding off these together.
+print
+
+# run it in the terminal
+
