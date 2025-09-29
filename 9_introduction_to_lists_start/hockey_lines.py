a57b25038ac25783e0b059eeb0defcb969d8aff0
Daniel Mouris <dgmouris@gmail.com>
2025-09-29 13:40:44 -0600

    class progress pushed

---
+++ 9_introduction_to_lists_start/hockey_lines.py
@@ -0,0 +1,15 @@
+# let's do a new example that will have the lines
+
+print("Some data about the oilers lines")
+# we're going have arrays of arrays
+oilers_forward_lines = [
+    ["Draisaitl", "Mcdavid", "Frederic"],
+    ["Howard", "Nugent-Hopkins", "Tomasek"],
+    ["Mangiapane", "Henrique", "Savoie"],
+    ["Podkolzin", "Philp", "Kapanen"]
+]
+
+# in here we're going make a checker that checks to see if a player is on a certain line
+# 1. get the input of the user of what line they want to check
+# 2. the player name they want to check
+# 3. display if the user is on that line or not.
