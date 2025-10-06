e1a070a1bb0acb3f92f94e3ca5c6f79e27dd79ec
Daniel Mouris <dgmouris@gmail.com>
2025-10-06 14:43:53 -0600

    class progress pushed

--- 15_for_loops_with_range_start/calculate_squares.py
+++ 15_for_loops_with_range_start/calculate_squares.py
@@ -8,5 +8,6 @@ STEP = 12
 
 # let's use this variables in our function
 # they are caps because they're viewed as constants
-for i in range(START, END, STEP):
-    print(F"value of i is {i} the square is {i**2}")
+for your_variable in range(START, END, STEP):
+    print(F"value of i is {your_variable} the square is {your_variable**2}")
+    # this is indented piece will execute each iteration.
---
+++ exercises_with_loops_and_exceptions_start/vowel_counter.py
@@ -0,0 +1,10 @@
+# step 1
+word = input("Enter a word for us to count vowels:")
+
+# I need to keep count of the vowels
+vowel_count = 0
+
+# what I'm going to do  is loop over the words' letters
+for letter in word:
+    print(letter)
+    # check if it's a vowel
