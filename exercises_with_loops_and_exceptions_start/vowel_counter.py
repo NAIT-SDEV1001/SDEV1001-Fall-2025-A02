f42cfc4d2f167630ca095325a32a934d10c34d86
Daniel Mouris <dgmouris@gmail.com>
2025-10-06 14:46:58 -0600

    class progress pushed

--- exercises_with_loops_and_exceptions_start/vowel_counter.py
+++ exercises_with_loops_and_exceptions_start/vowel_counter.py
@@ -3,8 +3,15 @@ word = input("Enter a word for us to count vowels:")
 
 # I need to keep count of the vowels
 vowel_count = 0
+VOWEL_LETTERS = "aieou" # we don't do y around here.
 
 # what I'm going to do  is loop over the words' letters
 for letter in word:
     print(letter)
     # check if it's a vowel
+    if letter in VOWEL_LETTERS:
+        # add one to the vowel count
+        vowel_count += 1 # vowel_count = vowel_count + 1
+
+# outside of the loop
+print(F"There are {vowel_count} vowels in the word {word}")
