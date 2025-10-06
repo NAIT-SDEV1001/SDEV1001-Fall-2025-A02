67522543739f551f10ac784fe2e3b267172c90d4
Daniel Mouris <dgmouris@gmail.com>
2025-10-06 13:33:30 -0600

    class progress pushed

---
+++ 14_lists_and_for_loops_start/lists_and_for_loops.py
@@ -0,0 +1,13 @@
+days_of_week = [
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday"
+]
+
+# Loop with the day of the week and the index
+# if the day has a u in it skip the day.
