882ab89051ea0123ce03adf43d6fc21f22ed796f
Daniel Mouris <dgmouris@gmail.com>
2025-10-06 13:43:45 -0600

    class progress pushed

--- 14_lists_and_for_loops_start/lists_and_for_loops.py
+++ 14_lists_and_for_loops_start/lists_and_for_loops.py
@@ -11,3 +11,17 @@ days_of_week = [
 
 # Loop with the day of the week and the index
 # if the day has a u in it skip the day.
+for index, day in enumerate(days_of_week):
+
+    # strings are character arrays in python as well
+    # as string which is really nice.
+    # if that's true we can use the "in" operator
+    # to check if something is in a string.
+    if "u" in day:
+        continue
+
+    # let's exit the loop on thurday
+
+    # adding a breakpoint
+    # breakpoint()
+    print(F"{index}: {day}")
