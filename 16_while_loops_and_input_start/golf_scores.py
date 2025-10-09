995886526400a7e47382987f880b0f3d9c5a6a7b
Daniel Mouris <dgmouris@gmail.com>
2025-10-09 13:37:38 -0600

    class progress pushed

--- 16_while_loops_and_input_start/golf_scores.py
+++ 16_while_loops_and_input_start/golf_scores.py
@@ -1,1 +1,11 @@
 print("Golf Score Calculator")
+
+# I want you to keep track of a couple of things
+# the amount of scores entered
+# the total score that the user entered
+
+# use a while loop to check if the user has entered
+# 1. a score (which you keep track of)
+# 2. if they wrote "quit" which exit the loop
+
+# at then end print the average score.
