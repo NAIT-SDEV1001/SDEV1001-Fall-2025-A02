b214abb9ffeba6cd1804f2b002e2c746768062df
Daniel Mouris <dgmouris@gmail.com>
2025-10-16 13:22:49 -0600

    class progress pushed

---
+++ 19_modules_and_functions_intro_start/from_the_slides.py
@@ -0,0 +1,7 @@
+# def is equivalent to function in javascript
+# def is saying hey I'm defining this function
+def greet_user():
+    print("hello")
+
+# you need to call it and use it for it do anyting
+greet_user()
