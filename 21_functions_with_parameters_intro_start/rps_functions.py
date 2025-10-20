f639dd2aef5c5c63d2f2de3d87339f8a23f65a88
Daniel Mouris <dgmouris@gmail.com>
2025-10-20 13:35:26 -0600

    class progress pushed

---
+++ 21_functions_with_parameters_intro_start/rps_functions.py
@@ -0,0 +1,21 @@
+
+# create a function called `translate_number_to_choice`
+# with the choice_number as an arguments
+# which will return
+# "scissor" for choice_number 0,
+# "rock" for choice_number 1
+# "paper" for choice_number 2
+# and the number otherwise.
+
+# use this function to get the user input string an
+# and the computer string.
+
+
+def get_game_result(
+    user_input,
+    computer_input
+):
+    ''' This is going to the result and print if the
+    user has won.
+    '''
+    # breakpoint()
