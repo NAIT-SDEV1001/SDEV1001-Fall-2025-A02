c8f718a1d004545a432280d2cdee073c53bb5816
Daniel Mouris <dgmouris@gmail.com>
2025-10-20 13:48:48 -0600

    class progress pushed

--- 21_functions_with_parameters_intro_start/rps_functions.py
+++ 21_functions_with_parameters_intro_start/rps_functions.py
@@ -1,11 +1,18 @@
 
 # create a function called `translate_number_to_choice`
-# with the choice_number as an arguments
-# which will return
-# "scissor" for choice_number 0,
-# "rock" for choice_number 1
-# "paper" for choice_number 2
-# and the number otherwise.
+def translate_number_to_choice(choice_number):
+    # with the choice_number as an arguments
+    # which will return
+    # "scissor" for choice_number 0,
+    if choice_number == 0:
+        return "scissor"
+    # "rock" for choice_number 1
+    if choice_number == 1:
+        return "rock"
+    # "paper" for choice_number 2
+    if choice_number == 2:
+        return "paper"
+    # and the number otherwise.
 
 # use this function to get the user input string an
 # and the computer string.
@@ -18,8 +25,18 @@ def get_game_result(
     ''' This is going to the result and print if the
     user has won.
     '''
-    pass
-    # breakpoint()
+    # use our function above
+    user_choice = translate_number_to_choice(user_input)
+    computer_choice = translate_number_to_choice(computer_input)
+    print("user_choice", user_choice)
+    print("computer_choice", computer_choice)
+
+    # to get the game result
+    # three scenarios
+    # tie if both results
+    # user wins
+    # computer wins
+
 
 
 
