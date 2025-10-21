4b59c4afdcf1dbe021b8bffa49826e962c0c28a4
Daniel Mouris <dgmouris@gmail.com>
2025-10-21 15:50:28 -0600

    class progress pushed

--- 22_functions_more_concepts_start/order_builder.py
+++ 22_functions_more_concepts_start/order_builder.py
@@ -11,16 +11,34 @@ def main():
     # users creating pizza
     ordering_pizza = True
     while ordering_pizza:
-        ordering = input("Order another pizza (y/n)")
+        ordering = input("Order another pizza (y/n) ")
         if ordering == "n":
             ordering_pizza = False
-            return # we could return early as a function.
-        size = int(input("what size do you want of pizza (number)?"))
-        crust = input("what type of crust do you want?")
+            # you could use return here
+            # you could use continue
+        elif ordering == "y":
+            size = int(input("what size do you want of pizza (number)? "))
+            crust = input("what type of crust do you want? ")
 
-        # let's actually call our pizza with key arguments
-        pizza.make_pizza(size=size, crust=crust)
+            # let's think about toppings
+            # you could have just a cheese pizza
+            # you could have cheese, pepperoni, peppers, onions
+            # you could have pineapple, no cheese, ham.
+            # there's a different amount of toppings.
+            toppings = []
+            topping = input("Add a topping type (done) when done: ")
+            while topping != "done":
+                # we're going to make this an array of toppings
+                toppings.append(topping)
+                topping = input("Add a topping type (done) when done: ")
 
+            breakpoint()
+
+            # let's actually call our pizza with key arguments
+            # we're just going to handle these toppings
+            pizza.make_pizza(size=size, crust=crust)
+        else:
+            print("I didn't get that.")
 # the right way to write files.
 if __name__ == "__main__":
     main()
