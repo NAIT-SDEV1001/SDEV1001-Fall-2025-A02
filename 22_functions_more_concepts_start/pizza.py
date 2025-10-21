8d6ee65a6fb4a937d5d811aa08c5c8ee4b86f1d2
Daniel Mouris <dgmouris@gmail.com>
2025-10-21 15:52:27 -0600

    class progress pushed

--- 22_functions_more_concepts_start/pizza.py
+++ 22_functions_more_concepts_start/pizza.py
@@ -1,9 +1,22 @@
 # in here we're going to create function
 
-def make_pizza(size, crust):
+# let's modify this so that we get all of the toppings
+# I'm going to do this with args.
+def make_pizza(*args, size=None, crust=None):
+    # args here is going to be our toppings.
+
     # make an f-string that can span multiple lines.
     print(F"""
     Summarizing the pizza
     a {size} inch pizza
     with a {crust}
     """)
+
+    print("""
+    Toppings to our pizza are as follows:
+    """)
+    # our args is going to be a list here.
+    for topping in args:
+        print(F"""
+        - {topping}
+        """)
