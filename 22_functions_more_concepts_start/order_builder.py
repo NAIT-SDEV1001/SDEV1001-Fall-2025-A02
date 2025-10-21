e6c2995b02e42a9d03064c4f09819981d4cab1a4
Daniel Mouris <dgmouris@gmail.com>
2025-10-21 15:44:18 -0600

    class progress pushed

--- 22_functions_more_concepts_start/order_builder.py
+++ 22_functions_more_concepts_start/order_builder.py
@@ -6,7 +6,20 @@ import pizza
 
 def main():
     print("Welcome to the order builder")
-    pass
+
+    # let's create a loop that's going to get
+    # users creating pizza
+    ordering_pizza = True
+    while ordering_pizza:
+        ordering = input("Order another pizza (y/n)")
+        if ordering == "n":
+            ordering_pizza = False
+            return # we could return early as a function.
+        size = int(input("what size do you want of pizza (number)?"))
+        crust = input("what type of crust do you want?")
+
+        # let's actually call our pizza with key arguments
+        pizza.make_pizza(size=size, crust=crust)
 
 # the right way to write files.
 if __name__ == "__main__":
