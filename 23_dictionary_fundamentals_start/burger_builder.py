751bd6933733b0ffc07b1913bb5020eff9eac784
Daniel Mouris <dgmouris@gmail.com>
2025-10-23 13:43:42 -0600

    class progress pushed

--- 23_dictionary_fundamentals_start/burger_builder.py
+++ 23_dictionary_fundamentals_start/burger_builder.py
@@ -5,3 +5,22 @@
 # cheese: true
 # toppings: lettuce, tomato, eggs
 # bun: ciabatta
+burger = {
+    "patties": 2,
+    "patty_type": "beef",
+    "cheese": True,
+    "toppings": [ # a list of items
+        "lettuce",
+        "tomato",
+        "eggs",
+    ],
+    "bun": "ciabatta"
+}
+
+# print out the dictionary.
+# print(burger)
+
+# I want you to print out a couple of things
+# THe number of patties and the patty type by accessing the
+# dictionary via keys
+# make if statement that will check if the use has cheese.
