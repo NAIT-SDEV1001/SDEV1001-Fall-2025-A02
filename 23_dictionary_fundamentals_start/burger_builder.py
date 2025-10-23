4ece5d2e1cd66a7782bdb0c3b78c865c84fd60a9
Daniel Mouris <dgmouris@gmail.com>
2025-10-23 14:08:04 -0600

    class progress pushed

--- 23_dictionary_fundamentals_start/burger_builder.py
+++ 23_dictionary_fundamentals_start/burger_builder.py
@@ -27,3 +27,12 @@ print(F"You have {burger['patties']} {burger['patty_type']} patties on your burg
 # make if statement that will check if the use has cheese.
 if burger["cheese"]: # it's true or false
     print("you have cheese on this burger")
+
+# I'm going to get you folks to list in order with the index
+# the toppings on the burger
+print("your toppings are")
+# for index, topping in enumerate(burger['toppings']): # burger['toppings'] is a list of strs.
+# the same as doing
+for index in range(len(burger["toppings"])):
+    topping = burger["toppings"][index]
+    print(F"- {index + 1}: {topping}")
