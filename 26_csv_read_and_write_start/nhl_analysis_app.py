ed86d32dc9da51b292aa5abd81413c95a6b9402e
Daniel Mouris <dgmouris@gmail.com>
2025-10-30 13:34:59 -0600

    class progress pushed

--- 26_csv_read_and_write_start/nhl_analysis_app.py
+++ 26_csv_read_and_write_start/nhl_analysis_app.py
@@ -4,15 +4,7 @@
 # dataset.
 # will return the list of dictionaries using the dictreader
 
-
-# create a main function
-# hard code the situations, prompt the for it
-# read all of the data
-# execute a breakpoint so that you folks can see the data.
-def main():
-    # filepaths relative to where you are in the
-    # folder structure
-    file_path = "data/nhl_teams_data_2024_2025.csv"
+def get_situation_from_user():
     situations = [
         "other",
         "all",
@@ -25,9 +17,22 @@ def main():
         label += F"- ({index}) {situation}\n"
     label += "(enter number): "
     print(label)
+    # convert this to a number
+    user_input_situation = int(input(label))
+    # as a note for future importment we could handle the errors
+    # that might pop up.
+    # return the string of the situation
+    return situations[user_input_situation]
 
-    user_input_situation = input(label)
-    print(user_input_situation)
+
+# create a main function
+# hard code the situations, prompt the for it
+# read all of the data
+# execute a breakpoint so that you folks can see the data.
+def main():
+    # filepaths relative to where you are in the
+    # folder structure
+    file_path = "data/nhl_teams_data_2024_2025.csv"
 
 
 # execute the main
