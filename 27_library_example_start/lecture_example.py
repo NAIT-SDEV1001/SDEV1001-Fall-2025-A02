be01f8001854ae2f54ba78093754e83089f13681
Daniel Mouris <dgmouris@gmail.com>
2025-11-03 13:49:44 -0700

    class progress pushed

--- 27_library_example_start/lecture_example.py
+++ 27_library_example_start/lecture_example.py
@@ -9,6 +9,25 @@ class Car:
         self.model = model
         self.year = year
 
+    # let's add a method here.
+    def honk(self): # self will always be the first param in python
+        print(F"{self.make} {self.model} is honking")
+
+    # to make our output in the code not look like
+    # <__main__.Car object at 0x000002A02ED76F90> for example
+    # we need to create a string method
+    def __str__(self):
+        return F"Car(make={self.make}, model={self.model}, year={self.year})"
+
 if __name__ == "__main__":
     first_car = Car("toyota", "corolla", 2006)
-    second_car = Car("Cadillac", "escalada", 2020)
+    second_car = Car("Cadillac", "escalade", 2020)
+
+    # when using the methods you're going to see that there's
+    # a difference in the objects when calling honk.
+    first_car.honk()
+    second_car.honk()
+
+    print("let's take a look at what the classes look like")
+    print(first_car)
+    print(second_car)
