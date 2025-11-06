04107ed1a458e0def70faa50d442c2f4eb350a97
Daniel Mouris <dgmouris@gmail.com>
2025-11-06 14:45:02 -0700

    class progress pushed

--- 28_course_example_start/tools/submissions.py
+++ 28_course_example_start/tools/submissions.py
@@ -3,6 +3,23 @@ class Submission:
         self.student = student # a Student instance
         self.assignment = assignment # a Assignment instance
         self.grade = grade # a float (decimal number)
+        self.letter_grade = self._assign_letter_grade() # needs to be under grade assignment.
+
+    # python has nothing that's private
+    # "_" denotes private function (should only be used in the class)
+    # this is a convention for variables and functions.
+    def _assign_letter_grade(self):
+        if self.grade > 90:
+            return "A"
+        if self.grade > 80:
+            return "B"
+        if self.grade > 70:
+            return "C"
+        if self.grade > 60:
+            return "D"
+        if self.grade <= 60:
+            return "F"
+        return "F"
 
     def __str__(self):
         return F"Student: {self.student.name} recieved grade {self.grade} on {self.assignment.name}"
