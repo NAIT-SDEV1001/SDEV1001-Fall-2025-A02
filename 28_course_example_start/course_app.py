7926de03381b72ae427c35e6ce69b59b32a29b7c
Daniel Mouris <dgmouris@gmail.com>
2025-11-13 14:10:26 -0700

    class progress pushed

--- 28_course_example_start/course_app.py
+++ 28_course_example_start/course_app.py
@@ -1,13 +1,17 @@
 # Import the class data using the data module.
-from data.course_data import student_data, assignment_data, submission_data
+from data.course_data import (
+    student_data,
+    assignment_data,
+    submission_data
+)
 
 from tools.course import Course
 from tools.assignment import Assignment
 from tools.student import Student
 
+# let's move this functionality into the class.
 def add_students(course):
     for student_info in student_data:
-        print(student_info)
         course.add_student(
             Student(id=student_info['id'],
                     name=student_info['name'])
@@ -15,15 +19,25 @@ def add_students(course):
 
 def add_assignments(course):
     for assignment_info in assignment_data:
-        print(assignment_info)
+
+        course.add_assignment(
+            Assignment(id=assignment_info['id'],
+                       name=assignment_info['name'])
+        )
 
 
 def main():
     course = Course("Dan's Engagement seminar for sleep")
-    print(course)
+    course.bulk_add_stduents(
+        student_data=student_data
+    )
+    course.bulk_add_assignments(
+        assignment_data=assignment_data
+    )
 
-    add_students(course)
-    add_assignments(course)
+    # add_students(course)
+    # add_assignments(course)
+    breakpoint()
 
 if __name__ == "__main__":
     main()
--- 28_course_example_start/tools/course.py
+++ 28_course_example_start/tools/course.py
@@ -1,10 +1,7 @@
-# create a Course class
-# constructor takes a single argument called name
-# create the variables on self.
-# name (str)
-# students (list)
-# a list (list)
-# create __str__ that says the name, number of students and assignments
+# the . here is saying look in this folder.
+from .student import Student
+from .assignment import Assignment
+
 class Course:
     def __init__(self, name):
         self.name = name
@@ -14,8 +11,24 @@ class Course:
     def add_student(self, student):
         self.students.append(student)
 
+    def bulk_add_stduents(self, student_data):
+        for student_info in student_data:
+            self.add_student(
+                Student(id=student_info['id'],
+                        name=student_info['name'])
+            )
+
     def add_assignment(self, assignment):
         self.assignments.append(assignment)
+
+    def bulk_add_assignments(self, assignment_data):
+        for assignment_info in assignment_data:
+            self.add_assignment(
+                Assignment(id=assignment_info['id'],
+                        name=assignment_info['name'])
+            )
+
+    # do the submissions piece.
 
     def __str__(self):
         return F"{self.name} has {len(self.students)} students and {len(self.assignments)} assignments"
