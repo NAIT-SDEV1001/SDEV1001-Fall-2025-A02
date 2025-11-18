d46d36831b2f7fe598f978ed8444ebc6785d225c
Daniel Mouris <dgmouris@gmail.com>
2025-11-18 16:24:03 -0700

    class progress pushed

--- 32_second_pypi_example_start/game_reviews.py
+++ 32_second_pypi_example_start/game_reviews.py
@@ -6,8 +6,22 @@
 # import pyexcel into this file as pex
 import pyexcel as pex # pex is just an alias for pyexcel
 
+def create_game_reviews_excel_file(data, output_file):
+    # use the pyexcel module.
+    pex
+
 def main():
-    pass
+    # name it this file name.
+    GAME_REVIEWS_FILE_NAME = "game_reviews.xlsx"
+    # take this data and convert into a xlsx file
+    GAME_REVIEWS = [
+        ["Title", "Platform", "Score"],
+        ["Elden Ring", "PC", 95],
+        ["Stardew Valley", "Switch", 89],
+        ["Cyberpunk 2077", "PS5", 82],
+        ["No Man's Sky", "PC", 90],
+        ["Gollum", "PS5", 43],
+    ]
 
 if __name__ == "__main__":
     main()
