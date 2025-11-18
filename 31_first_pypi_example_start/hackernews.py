071ce8495ff1cbfd7c9802053a7049c7bb5fbaaf
Daniel Mouris <dgmouris@gmail.com>
2025-11-18 15:41:01 -0700

    class progress pushed

--- 31_first_pypi_example_start/hackernews.py
+++ 31_first_pypi_example_start/hackernews.py
@@ -8,10 +8,18 @@ def get_top_ten_headlines():
     return headline_ids[:10]
 
 def get_news_story_data(id):
+    # if you're curious you can take a look at the docs here
+    # https://requests.readthedocs.io/en/latest/user/quickstart/
+    # on how to use requests, this is going to be how develop software.
     story_url = F"https://hacker-news.firebaseio.com/v0/item/{id}.json?print=pretty"
+    story_data = requests.get(story_url).json()
+    return story_data
+
 
 def main():
     top_ten_ids = get_top_ten_headlines()
+
+
     print("The hackernews top 10")
     print(top_ten_ids)
 
