0cb66b54adfdc72f7b96d92c55ef366866ef0720
Daniel Mouris <dgmouris@gmail.com>
2025-11-18 16:20:32 -0700

    class progress pushed

--- 32_second_pypi_example_start/game_reviews.py
+++ 32_second_pypi_example_start/game_reviews.py
@@ -1,5 +1,13 @@
-# create a virtual env
-# activate it
-# install pyexcel
-# install pyexcel-xlsx
+# create a virtual env: python -m venv venv
+# activate it: venv\Scripts\activate
+# install pyexcel: pip install pyexcel
+# install pyexcel-xlsx: pip install pyexcel-xlsx
+# make a requirements file: pip freeze > requirements.txt
 # import pyexcel into this file as pex
+import pyexcel as pex # pex is just an alias for pyexcel
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
