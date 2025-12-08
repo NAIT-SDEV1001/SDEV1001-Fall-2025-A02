bc3c318b758b26b49a20228d3347244e21bed31e
Daniel Mouris <dgmouris@gmail.com>
2025-12-08 13:36:02 -0700

    class progress pushed

--- 39_extra_lecture_inheritance_start/animals_app/animals.py
+++ 39_extra_lecture_inheritance_start/animals_app/animals.py
@@ -25,4 +25,20 @@ class Animal:
 
 # let's create a class that inherits from Animal
 class Dog(Animal):
-    pass
+    # If you just use pass below (and nothing else it's going to have the same
+    # functionality as animal.)
+    # pass
+
+    # let's override the init to have the same functionality
+    def __init__(self,  name, color, num_legs, breed):
+        # I want to use the constructor of the Animal class
+        # we're going to do this using something called "super()".
+        super().__init__(name, color, num_legs)
+        self.breed = breed
+
+    # let's continue to change the functionality but keep some the same.
+    def __str__(self):
+        # get the original animal string
+        animal_string = super().__str__()
+        # we're just adding the breed to it.
+        return F"{animal_string}\n Breed {self.breed}"
